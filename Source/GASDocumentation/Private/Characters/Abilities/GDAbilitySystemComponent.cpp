0804095be356b72d7978d3cb0132764af01ecc3a
Dan "Pan" Lott <danpan.nightshift@gmail.com>
2019-10-25 09:19:37 -0500

    Update GDAbilitySystemComponent.cpp

--- Source/GASDocumentation/Private/Characters/Abilities/GDAbilitySystemComponent.cpp
+++ Source/GASDocumentation/Private/Characters/Abilities/GDAbilitySystemComponent.cpp
@@ -3,13 +3,6 @@
 
 #include "GDAbilitySystemComponent.h"
 
-/*NOTE
-* FGameplayAbilityInputBinds REQUIRED a Correct Enum Name to bind to correctly. With a wrong enum name you will crash AFTER successfully binding input because unreal cannot find the enum in 
-* it's reflection system. If you do not have editor symbols installed it will give you a shit callstack, but the crash will come from
-* UEnum* GetBindEnum() { return FindObject<UEnum>(ANY_PACKAGE, *EnumName); } 
-* because it cannot find the enum listed.
-*/
-
 void UGDAbilitySystemComponent::ReceiveDamage(UGDAbilitySystemComponent * SourceASC, float UnmitigatedDamage, float MitigatedDamage)
 {
 	ReceivedDamage.Broadcast(SourceASC, UnmitigatedDamage, MitigatedDamage);
